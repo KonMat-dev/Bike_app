51fe4d768d797c4e5259a380bc20dda4c4c58d53
Konrad <61757091+KonMat-dev@users.noreply.github.com>
2021-12-30 20:44:40 +0100

    sql

--- Bike_app_v2/database.py
+++ Bike_app_v2/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./bike_app.db"
-SQLALCHEMY_DATABASE_URL = "postgres://hveqyyyhbigzeo:8781ef75adccf9c5909a907f4ff414fd1b95627b563cb1c90eb2389a8bd7dcee@ec2-99-81-177-233.eu-west-1.compute.amazonaws.com:5432/dao8eqkjta72db"
+SQLALCHEMY_DATABASE_URL = "postgresql://hveqyyyhbigzeo:8781ef75adccf9c5909a907f4ff414fd1b95627b563cb1c90eb2389a8bd7dcee@ec2-99-81-177-233.eu-west-1.compute.amazonaws.com:5432/dao8eqkjta72db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
