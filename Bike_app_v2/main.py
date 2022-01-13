eb3772848d686beb15df3bedbbd3080afb80f137
Konrad <61757091+KonMat-dev@users.noreply.github.com>
2022-01-13 19:21:31 +0100

    email nie dziłą

--- Bike_app_v2/main.py
+++ Bike_app_v2/main.py
@@ -34,7 +34,7 @@ conf = ConnectionConfig(
     MAIL_SERVER="smtp.gmail.com",
     MAIL_FROM_NAME="Desired Name",
     MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_SSL=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
